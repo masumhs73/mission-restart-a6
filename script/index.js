ba659a653442058012c495ec1d66c4c086455968
masumhs73 <masumshuvo58@gmail.com>
2026-02-17 22:56:44 +0600

    added footer section

--- script/index.js
+++ script/index.js
@@ -1,1 +1,10 @@
-console.log("index is connected")
+// console.log("index is connected")
+
+function loadCategories () {
+    fetch("https://fakestoreapi.com/products")
+    .then((res)=>res.json())
+    .then((data)=>console.log(data));
+
+}
+
+loadCategories();
